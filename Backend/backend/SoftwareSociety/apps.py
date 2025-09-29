a7c3f9b7a4bbd9f1e7d83ba50575ccec767d0891
Mango <126013734+Mangos489@users.noreply.github.com>
2025-09-28 19:18:33 -0700

    so hard
    Trying

--- Backend/backend/SoftwareSociety/apps.py
+++ Backend/backend/SoftwareSociety/apps.py
@@ -1,6 +1,6 @@
-# Backend/software_society/apps.py
 from django.apps import AppConfig
 
-class SoftwareSocietyConfig(AppConfig):
-    name = 'SoftwareSociety'
 
+class SoftwaresocietyConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'SoftwareSociety'
