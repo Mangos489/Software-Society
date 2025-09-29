2c46db0c2b7d3cd24e491f3c4db13e834b77babb
Mango <126013734+Mangos489@users.noreply.github.com>
2025-09-28 19:22:11 -0700

    Update settings.py

--- Backend/backend/backend/settings.py
+++ Backend/backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'softwaresociety.apps.SoftwaresocietyConfig',
+    'SoftwareSociety.apps.SoftwaresocietyConfig', # <-- Change this line
 ]
 
 MIDDLEWARE = [
