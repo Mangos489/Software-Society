b572ddad2b7e915cf6a44b7e3b49e634209713e3
Mango <126013734+Mangos489@users.noreply.github.com>
2025-10-13 12:19:13 -0700

    Update svelte.config.js

--- Software-Society/svelte.config.js
+++ Software-Society/svelte.config.js
@@ -11,11 +11,4 @@ const config = {
 	extensions: ['.svelte', '.svx']
 };
 
-export default {
-  kit: {
-    adapter: adapter({
-      // edge: true,   // optional: run SSR at the edge (Deno)
-      // split: true   // optional: split routes into multiple functions (not with edge)
-    })
-  }
-}
+export default config;
